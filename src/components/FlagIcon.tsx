7af449f0cbe1c9a3036ac29cf4066c00a0ed0963
EPScorp <90781058+EPScorp@users.noreply.github.com>
2024-02-28 09:43:32 +0000

    Update FlagIcon.tsx

--- src/components/FlagIcon.tsx
+++ src/components/FlagIcon.tsx
@@ -28,12 +28,8 @@ export function FlagIcon(props: FlagIconProps) {
 
   if (props.langCode === "minion")
     return (
-      <div className="w-8 h-6 rounded bg-[#ffff1a] flex justify-center items-center">
-        <div className="w-4 h-4 border-2 border-gray-500 rounded-full bg-white flex justify-center items-center">
-          <div className="w-1.5 h-1.5 rounded-full bg-gray-900 relative">
-            <div className="absolute top-0 left-0 w-1 h-1 bg-white rounded-full transform -translate-x-1/3 -translate-y-1/3" />
-          </div>
-        </div>
+      <div className="w-8 h-6 rounded bg-[#FFFFFF] flex justify-center items-center">
+        <img src="/flags/IMG_5872.svg" className="rounded" />
       </div>
     );
 
