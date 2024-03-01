ea96c004c9bd93579ac7377ebe89969edaef88ce
EPScorp <90781058+EPScorp@users.noreply.github.com>
2024-03-01 17:55:51 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -4,7 +4,7 @@ window.__CONFIG__ = {
   VITE_CORS_PROXY_URL: "",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NGMwNjg4MWRlYTE3ZjY0YWQ2NDVmNGUwODA5YWRlMSIsInN1YiI6IjY1ZGQxM2E2YzkyYzVkMDE2MzQ2ZDAzMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hJ7XOuKloHfmWFjtwzS9epJm08u3qC5CFA2ePWlNxU8",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
